bbf7f0aafc21489c716190dc67158dd25a45e6c6
v-bansal <65385023+v-bansal@users.noreply.github.com>
2020-05-18 13:32:35 +0530

    Add files via upload

--- attendance/recognizer.py
+++ attendance/recognizer.py
@@ -2,10 +2,11 @@ import cv2, numpy as np;
 import xlwrite,firebase.firebase_ini as fire;
 import time
 import sys
+from datetime import datetime;
 from playsound import playsound
 start=time.time()
 period=8
-face_cas = cv2.CascadeClassifier('haarcascade_profileface.xml')
+face_cas = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 cap = cv2.VideoCapture(0);
 recognizer = cv2.face.LBPHFaceRecognizer_create();
 recognizer.read('trainer/trainer.yml');
@@ -21,50 +22,34 @@ while True:
     ret, img = cap.read();
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
     faces = face_cas.detectMultiScale(gray, 1.3, 7);
+    newtime = datetime.now().time();
     for (x,y,w,h) in faces:
         roi_gray = gray[y:y + h, x:x + w]
         cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0),2);
         id,conf=recognizer.predict(roi_gray)
         if(conf < 50):
-         if(id==1):
-            id='Shubam Walia'
             if((str(id)) not in dict):
-                filename=xlwrite.output('attendance','class1',1,id,'yes');
+                filename=xlwrite.output('attendance','class1',1,id,newtime);
+                playsound('thank_you.mp3');
                 dict[str(id)]=str(id);
-                
-         elif(id==2):
-            id = 'Rohan'
-            if ((str(id)) not in dict):
-                filename =xlwrite.output('attendance', 'class1', 2, id, 'yes');
-                dict[str(id)] = str(id);
-
-         elif(id==3):
-            id = 'Raveen'
-            if ((str(id)) not in dict):
-                filename =xlwrite.output('attendance', 'class1', 3, id, 'yes');
-                dict[str(id)] = str(id);
-
-         elif(id==4):
-            id = 'Sonu'
-            if ((str(id)) not in dict):
-                filename =xlwrite.output('attendance', 'class1', 4, id, 'yes');
-                dict[str(id)] = str(id);
+            else :
+                time.sleep(3);
+                playsound('youarepresent.mp3');
 
         else:
              id = 'Unknown, can not recognize'
              flag=flag+1
-             break
-        
+             continue
         cv2.putText(img,str(id)+" "+str(conf),(x,y-10),font,0.55,(120,255,120),1)
         #cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,(0,0,255));
     cv2.imshow('frame',img);
     #cv2.imshow('gray',gray);
     if flag == 10:
-        playsound('transactionSound.mp3')
-        print("Transaction Blocked")
-        break;
-    if time.time()>start+period:
-        break;
+        playsound('try_again.mp3')
+        print("Try Again")
+    #    break;
+    #if time.time()>start+period:
+    #    break;
     if cv2.waitKey(100) & 0xFF == ord('q'):
         break;
 
